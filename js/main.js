a6b3781ddfa51224e8b9c4c08c943bccde3148dc
gerardosdl <gerardo.sdl.os@gmail.com>
2025-04-26 22:18:41 -0400

    Delete timer on html and add initial questions array on init function

--- js/main.js
+++ js/main.js
@@ -42,6 +42,68 @@ let maxScore;
 init();
 
 function init(){
+  questions = [
+    {
+    question: "What has a head, a tail, is brown, and has no legs?",
+    answers: ["A worm", "A snake", "A penny", "A dog toy"],
+    rightAnswer: 2,
+    chosenAnswer: null
+    },
+    {
+    question: "I have a little house in which I live all alone. It has no doors or windows, and if I want to go out I must break through the wall. What am I? ",
+    answers: ["A peanut", "A seed", "An egg", "A seashell"],
+    rightAnswer: 2,
+    chosenAnswer: null
+    },
+    {
+    question: "The more you take, the more you leave behind. What am I?",
+    answers: ["Sand", "Footsteps", "Time", "Memories"],
+    rightAnswer: 1,
+    chosenAnswer: null
+    },
+    {
+    question: "I have no mouth, so I cannot lie. I see the truth, without use of eyes. If you feel ready, then think carefully: What is it you see, when you look at me?",
+    answers: ["A dream", "A memory", "A painting", "A mirror"],
+    rightAnswer: 3,
+    chosenAnswer: null
+    },
+    {
+    question: "I am not your boss but I guide your decisions, I am not your trainer, but I dictate your pace, I am not a Judge but I measure your success. Who am I?",
+    answers: ["A goal", " A coach", "A clock", "A leader"],
+    rightAnswer: 0,
+    chosenAnswer: null
+    },
+    {
+    question: "I have a single waving hand I'm present in places across the land On fairer days, I depart I shorten with a broken heart. What am I?",
+    answers: ["A leaf", "A kite", "A windsock", " A flag"],
+    rightAnswer: 3,
+    chosenAnswer: null
+    },
+    {
+    question: "What 8 letter word can have a letter taken away and it still makes a word. Take another letter away and it still makes a word. Keep on doing that until you have one letter left. What is the word?",
+    answers: ["Smashing", "Starting", "Standing", "Shooting"],
+    rightAnswer: 1,
+    chosenAnswer: null
+    },
+    {
+    question: "What runs, but never walks. Murmurs, but never talks. Has a bed, but never sleeps. And has a mouth, but never eats?",
+    answers: ["A breeze", "A clock", "A road", "A river"],
+    rightAnswer: 3,
+    chosenAnswer: null
+    },
+    {
+    question: "Until I am measured, I am not known. Yet how you miss me when I have flown. What am I?",
+    answers: ["Sound", "Light", "Time", "Wind"],
+    rightAnswer: 2,
+    chosenAnswer: null
+    },
+    {
+    question: "Which month has 28 days?",
+    answers: ["Only February", "February and March", "Every month", "None of the above" ],
+    rightAnswer: 2,
+    chosenAnswer: null
+    },
+  ];
   
   render();
   }
@@ -49,4 +111,5 @@ function init(){
 function render(){
 
 }  
+// riddle questions's sources: https://www.riddles.com/ https://www.rd.com/article/riddles-for-adults/
 
